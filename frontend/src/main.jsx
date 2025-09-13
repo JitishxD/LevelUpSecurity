cd69fbbade9a4f67d48943d4bd2056d6f8ff932c
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2025-09-13 12:59:53 +0530

    Deployed To GithubPages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/LevelUpSecurity">
       <App />
     </BrowserRouter>
   </StrictMode>
